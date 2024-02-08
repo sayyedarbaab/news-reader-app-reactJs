9fafd67b64859cf36ef8feacbedb1ac46b1bc0d5
Sayyed Arbaab <kazmiwrite@gmail.com>
2024-02-08 14:31:16 +0530

    add commit

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,31 +1,39 @@
-import style from './Navbar.module.css'
+import style from "./Navbar.module.css";
 //problem solve
 // import { BrowserRouter as Router,Link} from 'react-router-dom'
-import { Link } from 'react-router-dom'
-import { Outlet } from 'react-router-dom'
+import { Link } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 
-
-function Navbar(){
-
-    return(
-        <div className={style.container}>
-        <nav className={style.nav}>
-            <div className={style.logo}>
-            <h1>Latest News</h1>
-            </div>
-            {/* <Router> */}
-            <ul>
-                <Link to='/' className={style.link}><h4>Home</h4></Link>
-                <Link to='/cricket_news' className={style.link}><h4>Cricket</h4></Link>
-                <Link to='/Education_news' className={style.link}><h4>Education</h4></Link>
-                <Link to='/Politics_news' className={style.link}><h4>Politics</h4></Link>
-            </ul>
-            <Link to='/LoginPage'><button className={style.btn}>login</button></Link>
-            {/* </Router> */}
-        </nav>
-        <Outlet />
+function Navbar() {
+  return (
+    <div className={style.container}>
+      <nav className={style.nav}>
+        <div className={style.logo}>
+          <h1>Nitin Gadkari</h1>
         </div>
-    )
+        {/* <Router> */}
+        <ul>
+          <Link to="/" className={style.link}>
+            <h4>Home</h4>
+          </Link>
+          <Link to="/cricket_news" className={style.link}>
+            <h4>About</h4>
+          </Link>
+          <Link to="/Education_news" className={style.link}>
+            <h4>Media</h4>
+          </Link>
+          <Link to="/Politics_news" className={style.link}>
+            <h4>Contact</h4>
+          </Link>
+        </ul>
+        <Link to="/LoginPage">
+          <button className={style.btn}>login</button>
+        </Link>
+        {/* </Router> */}
+      </nav>
+      <Outlet />
+    </div>
+  );
 }
 
-export default Navbar
+export default Navbar;
